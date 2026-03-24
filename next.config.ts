66883c973bd7a021d1794bce158caf2e4abb9d02
wan-elmus <elmuswangudi@gmail.com>
2026-03-24 17:53:05 +0300

    defined project root

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,25 @@
+// import type { NextConfig } from 'next'
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     formats: ['image/webp', 'image/avif'],
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+//     imageSizes: [16, 32, 48, 64, 96, 128, 256],
+//   },
+//   experimental: {
+//     optimizeCss: true,
+//   },
+//   devIndicators: false,
+// }
+
+// export default nextConfig
+
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: '/root/sensei/Mon-seh',
+  },
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
@@ -9,7 +28,6 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizeCss: true,
   },
-  devIndicators: false,
 }
 
 export default nextConfig
